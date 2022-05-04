0db0b217aa8ada839514bb9c68f85e1427176d51
Nimesh Kavinda <nimeshkavinda13@gmail.com>
2022-05-04 13:47:11 +0530

    add theme

---
+++ src/theme/colors.js
@@ -0,0 +1,10 @@
+const colors = {
+  primary: { bg: "#015EFF", text: "#fff" },
+  secondary: { bg: "#E5EBFF", text: "#015EFF" },
+  error: { primary: "#FF7A7D", secondary: "#FEE5E4" },
+  warning: { primary: "#FFC300", secondary: "#FFF2C9" },
+  success: { primary: "#50C878", secondary: "#E1F9E8" },
+  grey: { dark: "#3B3B3B", medium: "#dcdcdc", light: "#f4f4f4" },
+};
+
+export default colors;

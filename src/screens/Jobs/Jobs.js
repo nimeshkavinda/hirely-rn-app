3a2956bc08874ea207e224b8e554a92193ee6b78
Nimesh Kavinda <nimeshkavinda13@gmail.com>
2022-05-07 16:14:18 +0530

    add search

--- src/screens/Jobs/Jobs.js
+++ src/screens/Jobs/Jobs.js
@@ -1,8 +1,17 @@
 import React from "react";
-import { Text } from "react-native";
+import { View, SafeAreaView } from "react-native";
+import styles from "./styles";
+import { Text, Input } from "../../common";
 
 const Jobs = () => {
-  return <Text>Jobs</Text>;
+  return (
+    <SafeAreaView style={styles.wrapper}>
+      <View style={styles.searchWrapper}>
+        <Input style={styles.search} placeholder="Job title, type or keyword" />
+        <Text style={styles.resultsText}>200 job openings</Text>
+      </View>
+    </SafeAreaView>
+  );
 };
 
 export default Jobs;
---
+++ src/screens/Jobs/styles.js
@@ -0,0 +1,26 @@
+import { StyleSheet, Platform, StatusBar } from "react-native";
+import colors from "../../theme/colors";
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    backgroundColor: "#fff",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+  },
+  searchWrapper: {
+    marginVertical: 20,
+    borderBottomColor: "#f6f6f6",
+    borderBottomWidth: 1,
+    paddingBottom: 20,
+  },
+  search: { marginHorizontal: 20 },
+  resultsText: {
+    fontSize: 16,
+    fontWeight: "500",
+    lineHeight: 18,
+    marginTop: 16,
+    marginHorizontal: 20,
+  },
+});
+
+export default styles;
